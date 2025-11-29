9244ad1ca8a0ba9793b6d20f17b7eb21264deb22
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-11-29 13:41:29 +0000

    Home Page: fixed link reference

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <div className="p-8">
             <h1 className="text-3xl font-bold">Welcome to Puzzle Paddy</h1>
-            <p className="mt-4">Visit the <a href="/tictactoe" className="text-blue-600 underline">Tic-Tac-Toe</a> game.</p>
+            <p className="mt-4">Play <Link href="/tictactoe" className="text-blue-600 underline">Tic-Tac-Toe</Link> ?.</p>
         </div>
     );
 }
