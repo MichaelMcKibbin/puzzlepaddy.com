e25015f41129208f8a32e7f6eae646b48c83d2f6
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-11-29 16:13:01 +0000

    Home Page: fixed link references - trailing / - to fix 404 errors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // important for `next export`
-    // basePath: '',    // only if you ever host under a subfolder
+    output: 'export',
+    trailingSlash: true,
+    distDir: 'out',
+    images: {
+        unoptimized: true
+    }
 };
 
 module.exports = nextConfig;
