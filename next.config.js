4720d9e705363c8a220259df0fb5313928bc0d55
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-12-04 20:07:47 +0000

    layout: adjust layout of games page

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // Only use export mode for production builds
+    ...(process.env.NODE_ENV === 'production' && {
+        output: 'export',
+        distDir: 'out'
+    }),
     trailingSlash: true,
-    distDir: 'out',
     images: {
         unoptimized: true
     }
