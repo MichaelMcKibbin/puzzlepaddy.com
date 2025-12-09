9cbc3c462c8c3a993b8c8644311e3c9438dc8589
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-12-09 02:44:48 +0000

    fix: update next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Only use export mode for production builds
-    ...(process.env.NODE_ENV === 'production' && {
-        output: 'export',
-        distDir: 'out'
-    }),
     trailingSlash: true,
     images: {
         unoptimized: true
