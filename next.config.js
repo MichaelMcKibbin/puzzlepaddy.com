8c6b86583a2d459c78d55cb595ae77d2dc2c62c4
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-12-09 06:24:06 +0000

    fix: expose env vars via next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     trailingSlash: true,
     images: {
         unoptimized: true
+    },
+    env: {
+        EMAIL_HOST: process.env.EMAIL_HOST,
+        EMAIL_PORT: process.env.EMAIL_PORT,
+        EMAIL_USER: process.env.EMAIL_USER,
+        EMAIL_PASS: process.env.EMAIL_PASS,
     }
 };
 
