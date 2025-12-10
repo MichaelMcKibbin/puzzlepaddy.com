eae45a5609474e1e1ce97a5d76c31b38ddbc3338
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-12-10 03:38:30 +0000

    config: edit env block to make more secure.

--- next.config.js
+++ next.config.js
@@ -9,9 +9,7 @@ const nextConfig = {
         EMAIL_HOST: process.env.EMAIL_HOST,
         EMAIL_PORT: process.env.EMAIL_PORT,
         EMAIL_USER: process.env.EMAIL_USER,
-        EMAIL_PASS: process.env.EMAIL_PASS,
         NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-        RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
     }
 };
 
